8b955c93d5ad6bb1800745d14840185ea865b71f
Rubal0990 <rubal00009@gmail.com>
2021-06-16 23:02:44 +0530

    Object Detection & Optical Character Recognition

---
+++ Task 1 - Object Detection_Optical Character Recognition/OD_ORC.py
@@ -0,0 +1,66 @@
+import cv2
+import numpy as np
+import pytesseract
+from PIL import ImageGrab
+
+thres = 0.45
+nms_threshold = 0.2
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+cap.set(10, 150)
+
+classNames = []
+classFile = 'coco.names'
+with open(classFile, 'rt') as f:
+    classNames = f.read().rstrip('\n').split('\n')
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+configPath = 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt'
+weightsPath = 'frozen_inference_graph.pb'
+
+net = cv2.dnn_DetectionModel(weightsPath, configPath)
+net.setInputSize(320, 320)
+net.setInputScale(1.0 / 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+def captureScreen(bbox=(300, 300, 1500, 1000)):
+    capScr = np.array(ImageGrab.grab(bbox))
+    capScr = cv2.cvtColor(capScr, cv2.COLOR_RGB2BGR)
+    return capScr
+
+
+while True:
+    timer = cv2.getTickCount()
+    success, img = cap.read()
+    img = cv2.resize(img, (0, 0), None, 0.7, 0.7)
+    classIds, confs, bbox = net.detect(img, confThreshold=thres)
+    bbox = list(bbox)
+    confs = list(np.array(confs).reshape(1, -1)[0])
+    confs = list(map(float, confs))
+
+    # DETECTING CHARACTERS
+    hImg, wImg, _ = img.shape
+    boxes = pytesseract.image_to_boxes(img)
+    for b in boxes.splitlines():
+        b = b.split(' ')
+        x, y, w, h = int(b[1]), int(b[2]), int(b[3]), int(b[4])
+        cv2.rectangle(img, (x, hImg - y), (w, hImg - h), (50, 50, 255), 2)
+        cv2.putText(img, b[0], (x, hImg - y + 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (50, 50, 255), 2)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+
+    # DETECTING OBJECTS
+    indices = cv2.dnn.NMSBoxes(bbox, confs, thres, nms_threshold)
+    for i in indices:
+        i = i[0]
+        box = bbox[i]
+        x, y, w, h = box[0], box[1], box[2], box[3]
+        cv2.rectangle(img, (x, y), (x + w, h + y), color=(0, 255, 0), thickness=2)
+        cv2.putText(img, classNames[classIds[i][0] - 1].upper(), (box[0] + 10, box[1] + 30),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+
+    cv2.imshow("Output", img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
